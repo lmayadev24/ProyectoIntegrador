fde52b0adfdeb7423f127070ced837a4d5ded045
Luis Angel Maya <lmayadev24@gmail.com>
2025-11-26 18:05:40 -0600

    Add Modificar nivel

--- PHP/modificar.php
+++ PHP/modificar.php
@@ -2,15 +2,15 @@
     require "conexion.php";
 
     $con = conectar();
-    $valor=$_GET['user'];
-    $restric = $_GET['nivel'];
+    $user=$_GET['user'];
+    $nivel = $_GET['nivel'];
 
-    if($restric === 'Administrador'){
+    if($nivel === 'Administrador'){
         header("Location: ../html/dashboard.php");
         exit;
     } else{
-        $elimina = "delete from usuarios where Usuario = '".$valor ."'";
-        $con->query($elimina);
+        $modificar = "UPDATE usuarios SET nivel = '".$nivel ."' WHERE Usuario = '".$user ."'";
+        $con->query($modificar);
         $con->close();
 
         header("Location: ../html/dashboard.php");
